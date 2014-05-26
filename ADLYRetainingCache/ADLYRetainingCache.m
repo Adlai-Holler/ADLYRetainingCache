e6803c7ecae43ee6c6d173c6850e3304a031d3ee
Adly <adly@apportable.com>
2014-05-26 14:24:05 -0700

    use one lock

--- ADLYRetainingCache/ADLYRetainingCache.m
+++ ADLYRetainingCache/ADLYRetainingCache.m
@@ -1,17 +1,12 @@
 #import "ADLYRetainingCache.h"
 
-#define WILL_ACCESS_RETAINED_PAIRS [_retainedPairsLock lock]
-#define DID_ACCESS_RETAINED_PAIRS [_retainedPairsLock unlock]
-
-#define WILL_ACCESS_RETENTION_KEYS [_retentionKeysLock lock]
-#define DID_ACCESS_RETENTION_KEYS [_retentionKeysLock unlock]
+#define WILL_ACCESS_DATA [_dataLock lock]
+#define DID_ACCESS_DATA [_dataLock unlock]
 
 @interface ADLYRetainingCache()
 @property (nonatomic, strong) NSMapTable *retainedPairs;
-@property (nonatomic, strong) NSLock *retainedPairsLock;
+@property (nonatomic, strong) NSLock *dataLock;
 @property (nonatomic, strong) NSMapTable *retentionKeys;
-/** This lock also guards the mutable set values in retentionKeys */
-@property (nonatomic, strong) NSLock *retentionKeysLock;
 @end
 
 @implementation ADLYRetainingCache
@@ -20,99 +15,92 @@
     self = [super init];
     if (!self) return nil;
     _retainedPairs = [NSMapTable strongToStrongObjectsMapTable];
-    _retainedPairsLock = [[NSLock alloc] init];
     _retentionKeys = [NSMapTable strongToStrongObjectsMapTable];
-    _retentionKeysLock = [[NSLock alloc] init];
+    _dataLock = [[NSLock alloc] init];
     return self;
 }
 
-- (id)objectForKey:(id)key {
+- (id)_objectForKey:(id)key lock:(BOOL)lock {
     id s = [super objectForKey:key];
     if (!s) {
-        WILL_ACCESS_RETAINED_PAIRS;
+        if (lock) WILL_ACCESS_DATA;
         s = [_retainedPairs objectForKey:key];
-        DID_ACCESS_RETAINED_PAIRS;
+        if (lock) DID_ACCESS_DATA;
     }
     return s;
+}
+
+- (id)objectForKey:(id)key {
+    return [self _objectForKey:key lock:YES];
 }
 
 - (void)removeObjectForKey:(id)key {
     [super removeObjectForKey:key];
     if (!key) return;
     
-    WILL_ACCESS_RETAINED_PAIRS;
+    WILL_ACCESS_DATA;
     [_retainedPairs removeObjectForKey:key];
-    DID_ACCESS_RETAINED_PAIRS;
-    
-    WILL_ACCESS_RETENTION_KEYS;
     [_retentionKeys removeObjectForKey:key];
-    DID_ACCESS_RETENTION_KEYS;
+    DID_ACCESS_DATA;
 }
 
 - (void)retainKey:(id)key withRetentionKey:(id)retentionKey {
     if (!key) return;
     NSParameterAssert(retentionKey != nil);
-    id object = [self objectForKey:key];
-    if (object == nil) return;
     
-    WILL_ACCESS_RETENTION_KEYS;
+    WILL_ACCESS_DATA;
+    id object = [self _objectForKey:key lock:NO];
+    if (object == nil) {
+        DID_ACCESS_DATA;
+        return;
+    }
+
     NSMutableSet *retentions = [_retentionKeys objectForKey:key];
     if (!retentions) {
-        retentions = [NSMutableSet setWithObject:retentionKey];
+        retentions = [[NSMutableSet alloc] init];
         [_retentionKeys setObject:retentions forKey:key];
-    } else {
-        [retentions addObject:retentionKey];
     }
-    BOOL isFirstRetain = (retentions.count == 1);
-    DID_ACCESS_RETENTION_KEYS;
-    
-    if (isFirstRetain) {
-        WILL_ACCESS_RETAINED_PAIRS;
+    if (retentions.count == 0) {
         [_retainedPairs setObject:object forKey:key];
-        DID_ACCESS_RETAINED_PAIRS;
     }
+    [retentions addObject:retentionKey];
+    DID_ACCESS_DATA;
 }
 
 - (void)forceReleaseKey:(id)key {
     if (!key) return;
-    WILL_ACCESS_RETENTION_KEYS;
+    WILL_ACCESS_DATA;
     [_retentionKeys removeObjectForKey:key];
-    DID_ACCESS_RETENTION_KEYS;
     
-    WILL_ACCESS_RETAINED_PAIRS;
     id object = [_retainedPairs objectForKey:key];
-    DID_ACCESS_RETAINED_PAIRS;
+    DID_ACCESS_DATA;
 
     if (object) [super setObject:object forKey:key];
 }
 
 - (NSInteger)retainCountForKey:(id)key {
-    WILL_ACCESS_RETENTION_KEYS;
+    WILL_ACCESS_DATA;
     NSInteger result = [[_retentionKeys objectForKey:key] count];
-    DID_ACCESS_RETENTION_KEYS;
+    DID_ACCESS_DATA;
     return result;
 }
 
 - (void)releaseKey:(id)key withRetentionKey:(id)retentionKey {
     if (!key) return;
     NSParameterAssert(retentionKey);
-    id object = [self objectForKey:key];
+    WILL_ACCESS_DATA;
     
-    WILL_ACCESS_RETENTION_KEYS;
     NSMutableSet *retentions = [_retentionKeys objectForKey:key];
-    [retentions removeObject:retentionKey];
-    BOOL lastRelease = (0 == retentions.count);
-    if (lastRelease) {
-        [_retentionKeys removeObjectForKey:key];
+    if (retentions.count > 0) {
+        [retentions removeObject:retentionKey];
+        if (0 == retentions.count) {
+            [_retentionKeys removeObjectForKey:key];
+            id object = [_retainedPairs objectForKey:key];
+            if (object) [super setObject:object forKey:key];
+            [_retainedPairs removeObjectForKey:key];
+        }
     }
-    DID_ACCESS_RETENTION_KEYS;
-    
-    if (lastRelease) {
-        if (object) [super setObject:object forKey:key];
-        WILL_ACCESS_RETAINED_PAIRS;
-        [_retainedPairs removeObjectForKey:key];
-        DID_ACCESS_RETAINED_PAIRS;
-    }
+    DID_ACCESS_DATA;
 }
 
 @end
